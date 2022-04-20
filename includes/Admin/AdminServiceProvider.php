45c57097b34af1390b4e03e32d9a325756a7715c
Zabir Anik <xaviranik.me@gmail.com>
2022-04-20 13:20:29 +0600

    add: admin service provider

---
+++ includes/Admin/AdminServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WpPluginSkeleton\Admin;
+
+/**
+ * Class AdminServiceProvider
+ *
+ * Handles the service provider for the admin area.
+ *
+ * @package WpPluginSkeleton\Admin
+ */
+class AdminServiceProvider {
+}
