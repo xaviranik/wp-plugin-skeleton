70d56764abff0a32bb3f79d10276fe92c35fa1c5
Zabir Anik <xaviranik.me@gmail.com>
2022-04-22 13:22:32 +0600

    add: frontend service provider

---
+++ includes/Frontend/FrontendServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WpPluginSkeleton\Frontend;
+
+/**
+ * Class FrontendServiceProvider
+ *
+ * Handles the services for the frontend.
+ *
+ * @package WpPluginSkeleton\Frontend
+ */
+class FrontendServiceProvider {
+
+}
