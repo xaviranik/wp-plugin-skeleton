92349054e7fcf59f2fc47b1d582d69fda3484fe5
Zabir Anik <xaviranik.me@gmail.com>
2024-03-04 13:32:27 +0600

    add: wp scripts build process

---
+++ assets/src/index.js
@@ -0,0 +1,6 @@
+// Import stylesheet for main styles.
+import './styles/main.scss';
+
+// Import JavaScript files for additional functionality.
+// These files may contain logic, utilities, or modules required by your application.
+// Ensure to import them in the desired order to resolve dependencies correctly.
